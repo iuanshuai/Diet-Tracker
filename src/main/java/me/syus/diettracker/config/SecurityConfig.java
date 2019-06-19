202637b8842c66d368296a2d831021dccd383172
Shuai Yuan <ishuaiyuan@gmail.com>
2019-06-19 12:17:06 -0400

    EntryPoint

--- src/main/java/me/syus/diettracker/config/SecurityConfig.java
+++ src/main/java/me/syus/diettracker/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.syus.diettracker.config;
 
 
+import me.syus.diettracker.extend.security.RestAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,20 +14,41 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+//    // Step 1
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user1").password("{noop}password").roles("xxxx");
+//    }
+//
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        http.csrf().disable() // cross-site request forgery
+//                .authorizeRequests()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin();
+//    }
 
-
-
-    public void configureGlobal(@Autowired AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user1").password("{noop}password").roles("xxxx");
+    // Step 2
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("{noop}password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user2").password("{noop}password").roles("ADMIN");
     }
 
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.csrf().disable() // cross-site request forgery
-                .authorizeRequests()
-                .anyRequest().authenticated()
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/api/users").permitAll()
                 .and()
-                .formLogin();
+                .authorizeRequests().antMatchers("/api/**").hasAnyRole("REGISTERED_USER","ADMIN")
+                .and()
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint);
     }
+
+
+
+
 
 }
